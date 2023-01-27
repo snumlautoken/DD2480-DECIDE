0444f31f89f9a72ef6af791cda5f4e7d5d4dad71
Felix Qvarfordt <felixq@kth.se>
2023-01-27 14:47:27 +0100

    Fixed input static type
    Connectors matrix and numpoint variables changed to static to allow us
    to use all inputs without instantiation.

--- DECIDE/src/main/java/Group2/Input.java
+++ DECIDE/src/main/java/Group2/Input.java
@@ -4,7 +4,7 @@ public class Input {
     public enum Connector {
         ANDD, ORR, NOTUSED
     }
-    public int NUMPOINTS;
-    public Connector[][] LCM = new Connector[15][15];
+    public static int NUMPOINTS;
+    public static Connector[][] LCM = new Connector[15][15];
     public boolean[] PUV = new boolean[15];
 }
