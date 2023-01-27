ec1ff52b5241f0a68ef0825c903be4120868879b
Felix Qvarfordt <felixq@kth.se>
2023-01-27 15:19:56 +0100

    Added coordinates to input
    Added coordinates to input class as an array of 100 static java awt points.
    Initially we said class but that should not be needed if i am not
    missing something.

--- DECIDE/src/main/java/Group2/Input.java
+++ DECIDE/src/main/java/Group2/Input.java
@@ -1,5 +1,7 @@
 package Group2;
 
+import java.awt.*;
+
 public class Input {
     public enum Connector {
         ANDD, ORR, NOTUSED
@@ -7,4 +9,6 @@ public class Input {
     public int NUMPOINTS;
     public Connector[][] LCM = new Connector[15][15];
     public boolean[] PUV = new boolean[15];
+
+    public static Point[] Coordinates = new Point[100];
 }
