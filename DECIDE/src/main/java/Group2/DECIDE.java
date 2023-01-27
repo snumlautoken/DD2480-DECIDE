f16460ad921851d58743336d724e277b02cd5db7
Arami Alfarhani <aramia@kth.se>
2023-01-27 12:19:03 +0100

    Intialize class

--- DECIDE/src/main/java/Group2/DECIDE.java
+++ DECIDE/src/main/java/Group2/DECIDE.java
@@ -1,8 +1,7 @@
 package Group2;
 
 public class DECIDE {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    boolean launch;
+    public void decide() {
     }
 }
