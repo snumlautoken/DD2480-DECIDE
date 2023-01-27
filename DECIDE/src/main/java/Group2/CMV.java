9396f45bb56b522bb00d35c5496063c5099bd418
Oskar Svanström <oskarsva@kth.se>
2023-01-27 12:26:05 +0100

    Created cmv class
    Static cmv-array of length 15. Empty LIC functions to be implemented.
    Also a function for calling all LICs is implemented

---
+++ DECIDE/src/main/java/Group2/CMV.java
@@ -0,0 +1,70 @@
+package Group2;
+
+public class CMV{
+    public static boolean[] cmv = new boolean[15];
+
+    // Calls all LIC calculcations
+    public static void calculate(){
+        calcLIC0();
+        calcLIC1();
+        calcLIC2();
+        calcLIC3();
+        calcLIC4();
+        calcLIC5();
+        calcLIC6();
+        calcLIC7();
+        calcLIC8();
+        calcLIC9();
+        calcLIC10();
+        calcLIC11();
+        calcLIC12();
+        calcLIC13();
+        calcLIC14();
+    }
+
+    // TODO!
+    public static void calcLIC0(){}
+
+    // TODO!
+    public static void calcLIC1(){}
+
+    // TODO!
+    public static void calcLIC2(){}
+
+    // TODO!
+    public static void calcLIC3(){}
+
+    // TODO!
+    public static void calcLIC4(){}
+
+    // TODO!
+    public static void calcLIC5(){}
+
+    // TODO!
+    public static void calcLIC6(){}
+
+    // TODO!
+    public static void calcLIC7(){}
+
+    // TODO!
+    public static void calcLIC8(){}
+
+    // TODO!
+    public static void calcLIC9(){}
+
+    // TODO!
+    public static void calcLIC10(){}
+
+    // TODO!
+    public static void calcLIC11(){}
+
+    // TODO!
+    public static void calcLIC12(){}
+
+    // TODO!
+    public static void calcLIC13(){}
+
+    // TODO!
+    public static void calcLIC14(){}
+
+}
