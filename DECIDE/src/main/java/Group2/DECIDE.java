b9291741aef7a81f7f37d6465e5fcf9c3af82aae
snumlautoken <arami@live.se>
2023-01-28 20:41:17 +0100

    Finish FUV and launch

--- DECIDE/src/main/java/Group2/DECIDE.java
+++ DECIDE/src/main/java/Group2/DECIDE.java
@@ -5,6 +5,7 @@ public class DECIDE {
     public static void decide() {
         CMV.calculate();
         boolean[][] PUM = new boolean[15][15];
+        boolean[] FUV = new boolean[15];
         for (int i = 0; i < 15; i++) {
             for (int j = i; j < 15; j++) {
                 PUM[i][j] = switch (Input.LCM[i][j]) {
@@ -19,7 +20,17 @@ public class DECIDE {
                 };
                 PUM[j][i] = PUM[i][j];
             }
+            FUV[i] = !Input.PUV[i] || allIsTrue(PUM[i]);
         }
 
+        launch = allIsTrue(FUV);
+
+        String yN = launch ? "Yes" : "No";
+        System.out.println(yN);
+    }
+    
+    private static boolean allIsTrue(boolean[] array) {
+        for(boolean b : array) if(!b) return false;
+        return true;
     }
 }
