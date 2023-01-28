03813551cedbb17fbc5f7b3b61bfb087e3969311
snumlautoken <arami@live.se>
2023-01-28 20:25:21 +0100

    Make decide and launch static

--- DECIDE/src/main/java/Group2/DECIDE.java
+++ DECIDE/src/main/java/Group2/DECIDE.java
@@ -1,8 +1,8 @@
 package Group2;
 
 public class DECIDE {
-    boolean launch;
-    public void decide() {
+    public static boolean launch;
+    public static void decide() {
         CMV.calculate();
         boolean[][] PUM = new boolean[15][15];
         for (int i = 0; i < 15; i++) {
