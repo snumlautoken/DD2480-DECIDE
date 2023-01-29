f9e748130c14b7d0204a9f5a29028babcda18e99
Jakob Ewaldsson <jakobewaldsson@gmail.com>
2023-01-29 18:29:43 +0100

    Added tests for LIC function 0

--- DECIDE/src/test/java/Tests.java
+++ DECIDE/src/test/java/Tests.java
@@ -44,6 +44,24 @@ public class Tests {
     }
 
     @Test
+    public void TestLIC0(){
+        Input.Coordinates[0].setLocation(0, 0);
+        Input.Coordinates[1].setLocation(1, 0);
+        Input.NUMPOINTS = 2;
+        Input.Parameters.LENGTH1 = 0.0;
+
+        CMV.calcLIC0();
+        assertTrue(CMV.cmv[0], "Error! LIC0 should be true since the distance is larger than 0");
+
+        Input.Coordinates[2].setLocation(1, 1);
+        Input.NUMPOINTS = 3;
+        Input.Parameters.LENGTH1 = 3.0;
+        assertTrue(!CMV.cmv[0], "Error! LIC0 should be false since the distances are smaller than 3");
+
+    }
+
+
+    @Test
     public void TestLIC2(){
         Input.Coordinates[0].setLocation(0, 0);
         Input.Coordinates[1].setLocation(0, 0);
