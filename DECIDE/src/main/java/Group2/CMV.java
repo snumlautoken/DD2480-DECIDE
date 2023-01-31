2fb336fecece460c30d880931bf5740e81a4db2b
OllanBollan <olivia.aronsson@gmail.com>
2023-01-31 03:14:32 +0100

    Revert "20 LIC 9 function"
    This reverts commit c986b9a4f32cf3068d830ff4c26737c88f09c93b.

--- DECIDE/src/main/java/Group2/CMV.java
+++ DECIDE/src/main/java/Group2/CMV.java
@@ -267,38 +267,7 @@ public class CMV{
     }
 
     // TODO!
-    public static void calcLIC9(){
-        cmv[9] = false;
-
-        if(Input.NUMPOINTS < 5){
-            return;
-        }
-
-        for (int i = 0; i < Input.NUMPOINTS-(Input.Parameters.CPTS+Input.Parameters.DPTS+2); i++) {
-            int bn = Input.Parameters.CPTS+1;
-            int bn2 = bn + Input.Parameters.DPTS+1;
-
-            Point p1 = Input.Coordinates[i];
-            Point p2 = Input.Coordinates[i+bn];
-            Point p3 = Input.Coordinates[i+bn2];
-
-            // If either the first point or the last point (or both) coincides with the vertex, the angle is undefined
-            if((doubleCompare(p1.x, p2.x)==Comptype.EQ&&doubleCompare(p1.y, p2.y)==Comptype.EQ)
-             ||(doubleCompare(p2.x, p3.x)==Comptype.EQ&&doubleCompare(p2.y, p3.y)==Comptype.EQ)){
-                continue;
-            }
-            
-            double angle = Math.acos((Math.pow(p2.distance(p1), 2)+Math.pow(p2.distance(p3), 2)-Math.pow(p1.distance(p3), 2))/
-            (2*p2.distance(p1)*p2.distance(p3)));
-            
-            if(doubleCompare(angle, Math.PI-Input.Parameters.EPSILON)==Comptype.LT || doubleCompare(angle, Math.PI+Input.Parameters.EPSILON)==Comptype.GT){
-                cmv[9] = true;
-                break;
-            }
-        }
-        return;
-
-    }
+    public static void calcLIC9(){}
 
     // TODO!
     public static void calcLIC10(){}
